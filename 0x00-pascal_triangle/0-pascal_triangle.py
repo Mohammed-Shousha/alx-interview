7043ef95078266564cc7e06691f646da24eca319
Mohammed Shousha <mshousha29@gmail.com>
2024-03-05 21:27:08 +0200

    0-pascal_triangle.py

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,9 @@
+def pascal_triangle(n):
+    triangle = [[1]]
+    for i in range(1, n):
+        triangle.append([1 if j in (0, i) else triangle[i-1]
+                        [j-1] + triangle[i-1][j] for j in range(i+1)])
+    return triangle if n > 0 else []
+
+
+print(pascal_triangle(10))
