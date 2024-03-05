08de3b19bb61b10063b998d57de1c25acb311ef4
Mohammed Shousha <mshousha29@gmail.com>
2024-03-05 21:34:13 +0200

    remove print

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -4,11 +4,11 @@
 
 
 def pascal_triangle(n):
+    '''Creates a list of lists of integers representing
+    the Pascal's triangle of a given integer.
+    '''
     triangle = [[1]]
     for i in range(1, n):
         triangle.append([1 if j in (0, i) else triangle[i-1]
                         [j-1] + triangle[i-1][j] for j in range(i+1)])
     return triangle if n > 0 else []
-
-
-print(pascal_triangle(10))
